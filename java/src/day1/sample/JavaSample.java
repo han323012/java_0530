41a3c5c7e93280c84369606901ba8e7037e50c43
han323012 <134986656+han323012@users.noreply.github.com>
2023-05-30 12:47:22 +0900

    java 헬로자바

--- java/src/day1/sample/JavaSample.java
+++ java/src/day1/sample/JavaSample.java
@@ -2,9 +2,17 @@ package day1.sample;
 
 public class JavaSample {
 
+	// 한국 주석 : 코드에 영향이 없는 설명 문구 
+	/* 여러줄 주석
+	 */
+	/** 메소드 설명하는 주석: 메소드를 배울 때 다시 설명
+	 * 
+	 * @param args
+	 */
+	// main :프로그램이 실행되기 위해 필요한 것 (메소드)
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 콘솔창에 원하는 문구를 출력하는 기능 (메소드) (sysout)
+		System.out.println("Hello Java!");
 	}
 
 }
