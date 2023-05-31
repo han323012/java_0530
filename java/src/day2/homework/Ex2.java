c2224d6648b620fa906682314bfb808ab6634403
han323012 <323han@naver.com>
2023-05-31 20:53:27 +0900

    java 숙제 완료

--- java/src/day2/homework/Ex2.java
+++ java/src/day2/homework/Ex2.java
@@ -1,8 +1,23 @@
 package day2.homework;
 
+import java.util.Scanner;
+
 public class Ex2 {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		System.out.println("두 정수를 입력하세요");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a + "+" + b + "=" + (a + b) );
+		System.out.println(a + "-" + b + "=" + (a - b) );
+		System.out.println(a + "*" + b + "=" + (a * b) );
+		System.out.println(a + "/" + b + "=" + ((double)a / b) );
+		System.out.println(a + "%" + b + "=" + (a % b) );
+		
+		
+		sc.close();
 		
 	}
 
