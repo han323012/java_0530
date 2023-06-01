d1db20acc68e2b06d7a6dede7b15bb4ce4e4b2e5
han323012 <134986656+han323012@users.noreply.github.com>
2023-06-01 15:15:19 +0900

    java 3일차 과제

---
+++ java/src/day3homework/Ex1_conditional.java
@@ -0,0 +1,16 @@
+package day3homework;
+
+import java.util.Scanner;
+
+public class Ex1_conditional {
+
+	public static void main(String[] args) {	
+		System.out.println("input gender : ");
+		Scanner sc = new Scanner(System.in);
+		int gen = sc.nextInt();
+		
+		
+		
+	}
+
+}
---
+++ java/src/day3homework/Ex2_if.java
@@ -0,0 +1,9 @@
+package day3homework;
+
+
+public class Ex2_if {
+
+	public static void main(String[] args) {
+	}
+
+}
