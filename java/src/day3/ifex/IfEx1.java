d9e50230507124d072870dff7287d7ada51d029f
han323012 <134986656+han323012@users.noreply.github.com>
2023-06-01 14:56:36 +0900

    java If문 예제

---
+++ java/src/day3/ifex/IfEx1.java
@@ -0,0 +1,21 @@
+package day3.ifex;
+
+public class IfEx1 {
+
+	public static void main(String[] args) {
+		// 정수 num거 0이면 0입니다라고 출력하는 코드
+		int num = 10;
+		if (num == 0) {
+			System.out.println("0 is 0");
+			
+		}
+		
+		// 정수 num이 0이 아니면 num is not 0라고 출력하는 코드를 작성하세요
+		if (num != 0) {
+			System.out.println(num + " is not 0");
+			
+		}
+		
+	}
+	
+}
