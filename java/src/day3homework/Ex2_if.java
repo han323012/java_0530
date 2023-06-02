e96e436c826e8c15d4e1317d6eb2ac5844574cfa
han323012 <134986656+han323012@users.noreply.github.com>
2023-06-02 14:34:40 +0900

    java 3일차 과제 풀이

--- java/src/day3homework/Ex2_if.java
+++ java/src/day3homework/Ex2_if.java
@@ -1,9 +1,26 @@
 package day3homework;
 
+import java.util.Scanner;
 
 public class Ex2_if {
 
 	public static void main(String[] args) {
+		char gender;
+		System.out.println("input gender : ");
+		Scanner sc = new Scanner(System.in);
+		gender = sc.next().charAt(0);
+		String result = "";
+		if(gender == 'M' ) {
+			//System.out.println("Are you are women? " + "Man");
+			result = "man";
+		}
+		if(gender == 'W') {
+			//System.out.println("Are you are women? " + "Women");
+			result = "Women";
+		}
+		System.out.println("Are you are women? " + result);
+		
+			sc.close();
+		}
 	}
 
-}
