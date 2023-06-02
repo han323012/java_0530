793eafd5d27d0b090d2a069fabdb375c629d9db3
han323012 <134986656+han323012@users.noreply.github.com>
2023-06-02 14:38:20 +0900

    java 이븐넘버

--- java/src/day4/ifex/EvenNumberEx2.java
+++ java/src/day4/ifex/EvenNumberEx2.java
@@ -4,10 +4,11 @@ public class EvenNumberEx2 {
 
 	public static void main(String[] args) {
 		//홀짝 판별 예제를 조건 연산자를 이용하여 작성
+		//이거 쓰면 간결해짐
 		
 		int num = 4;
 		String result = num % 2 == 0 ? "even" : "odd";
-		System.out.println(num + " is a" + result + " number.");
+		System.out.println(num + " is a " + result + " number.");
 		
 		
 		
