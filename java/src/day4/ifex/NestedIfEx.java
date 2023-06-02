c78791d5e7c7bb635a80e4b4f8c9ac2889eab890
han323012 <134986656+han323012@users.noreply.github.com>
2023-06-02 14:38:38 +0900

    java nested

--- java/src/day4/ifex/NestedIfEx.java
+++ java/src/day4/ifex/NestedIfEx.java
@@ -3,8 +3,22 @@ package day4.ifex;
 public class NestedIfEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//2,3,6의 배수 판번예제
+			// day4.pratice IfMultpleEx2문제
+		int num = 6;
+		if(num % 2 ==0) {
+			if(num % 3 == 0) 
+				System.out.println("6의 배수입니다.");
+			else {
+				System.out.println("2의 배수입니다.");
+			}
+		}
+		else if(num % 3 == 0) {
+			System.out.println("3의 배수입니다.");
+		}
+		else {
+			System.out.println("2,3,6의 배수가 아닙니다.");
+		}
 	}
 
 }
