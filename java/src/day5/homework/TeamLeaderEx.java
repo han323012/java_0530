22da3cee31a77a97e6227434a2f8e9a5cd8e703d
han323012 <134986656+han323012@users.noreply.github.com>
2023-06-05 15:17:07 +0900

    5일차 숙제

--- java/src/day5/homework/TeamLeaderEx.java
+++ java/src/day5/homework/TeamLeaderEx.java
@@ -1,14 +1,27 @@
 package day5.homework;
 
+import java.util.Scanner;
+
 public class TeamLeaderEx {
 
 	public static void main(String[] args) {
-		/* 30명의 학생이 있는 반에서 1번부터 연속된 번호를 가진 3명의 학생이 1조가 되도록 나누었다. 이 때 가장 큰 변호인 학생이 조장이 된다.
-		 * 학생 번호가 주엊ㅆ을 때 조장인지 조원인지 판별하는 코드를 작성하세요.
+		/* 30명의 학생이 있는 반에서 1번부터 연속된 번호를 가진 3명의 학생이 1조가 되도록 나누었다. 이 때 가장 큰 번호인 학생이 조장이 된다.
+		 * 학생 번호가 주었을 때 조장인지 조원인지 판별하는 코드를 작성하세요.
 		 * switch 문으로
 		 */
-		int
+		int num; 
 		
+		Scanner sc = new Scanner(System.in); 
+		num = sc.nextInt();
+		switch (num % 3) {
+		case 1 , 2 :
+			System.out.println("조원입니다.");
+		 break;
+		case 0 :
+			System.out.println("조장입니다.");
+			break;
+			
+		}
 	}
 
 }
