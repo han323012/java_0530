ff42a9799021b79b0fbf03cfa384da40bfe50628
han323012 <134986656+han323012@users.noreply.github.com>
2023-06-08 13:36:28 +0900

    수업

---
+++ java/src/day7/breakex/HelloworldEx.java
@@ -0,0 +1,17 @@
+package day7.breakex;
+
+public class HelloworldEx {
+
+	public static void main(String[] args) {
+		int i = 1;
+		while(true) {
+			System.out.println("hello world!");
+			if(i==5) {
+				break;
+				
+			}
+			i++;
+		}
+	}
+
+}
