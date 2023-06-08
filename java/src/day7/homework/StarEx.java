815c7f2e45c0554e4f04131bca19e40b37ea19fd
han323012 <134986656+han323012@users.noreply.github.com>
2023-06-08 14:42:29 +0900

    숙제

--- java/src/day7/homework/StarEx.java
+++ java/src/day7/homework/StarEx.java
@@ -6,28 +6,28 @@ public class StarEx {
 		
 		for(int i = 1; i <= 10; i++) {
 			
-			for(int j = 1; j <=5 - i; j++) {
+			for(int j = 1; j <=10 - i; j++) {
 				System.out.print(' ');
 				
 			}
-			for(int j=1; j <= i-5; j++) {
+			for(int j=1; j <= i; j++) {
 				System.out.print('*');
 					
 			}	
-			for(int j=1; j <= i-1; j++) {
+			for(int j=1; j <= i-5; j++) {
 				System.out.print('*');
 					
 			}	
 				
-			for(int j=1; j <= i-5; j++) {
+			for(int j=1; j <= i; j++) {
 				System.out.print('*');
 					
 			}	
-			for(int j=1; j <= i-6; j++) {
+			for(int j=1; j <=i-5; j++) {
 				System.out.print('*');
 					
 			}	
-			for(int j = 1; j <=5 - i; j++) {
+			for(int j = 1; j <=10 - i; j++) {
 				System.out.print(' ');
 				
 			}
