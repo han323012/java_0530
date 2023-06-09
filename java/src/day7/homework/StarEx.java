ff78b4fec8d79bb790edf36dea30c5d4f5758db4
han323012 <134986656+han323012@users.noreply.github.com>
2023-06-09 09:26:53 +0900

    7일차 숙제

--- java/src/day7/homework/StarEx.java
+++ java/src/day7/homework/StarEx.java
@@ -4,37 +4,32 @@ public class StarEx {
 
 	public static void main(String[] args) {
 		
-		for(int i = 1; i <= 10; i++) {
-			
-			for(int j = 1; j <=10 - i; j++) {
-				System.out.print(' ');
-				
-			}
-			for(int j=1; j <= i; j++) {
-				System.out.print('*');
-					
-			}	
-			for(int j=1; j <= i-5; j++) {
-				System.out.print('*');
-					
-			}	
-				
-			for(int j=1; j <= i; j++) {
-				System.out.print('*');
-					
-			}	
-			for(int j=1; j <=i-5; j++) {
-				System.out.print('*');
-					
-			}	
-			for(int j = 1; j <=10 - i; j++) {
-				System.out.print(' ');
-				
-			}
+		int num = 7;
+		for(int i = 1; i <=num; i++) {
+			int half = (num +1)/2;
+			if(i <= half) {
+				for(int j = 1; j <= half-i; j++) {
+					System.out.print(' ');
+				}
+				for(int j =1; j<= 2*i-1; j++) {
+					System.out.print('*');
+				}
 				System.out.println();
+			}
+			
+			else {
+				for(int j = 1; j <= i - half; j++) {
+					System.out.print(' ');
+				}
+				for(int j =1; j<=2*(num-i)+1; j++) {
+					System.out.print('*');
+				}
+				System.out.println();
+				
+			}
+			
 		}
+		
 	}
-	//밥
-	
 
 }
