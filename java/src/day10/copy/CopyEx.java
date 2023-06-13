bfca6fa7b4fdefb15823eb728e5342a0c8ecf3bc
han323012 <134986656+han323012@users.noreply.github.com>
2023-06-13 15:17:34 +0900

    10일차 공부

--- java/src/day10/copy/CopyEx.java
+++ java/src/day10/copy/CopyEx.java
@@ -14,12 +14,15 @@ public class CopyEx {
 		/* 창조 변수는 값을 복사하면 주소가 복사되기 때문에
 		 * 공유가 됨
 		 */
-		
+		System.out.println("====================");
 		A a1 = new A();
 		A a2 = a1;
 		System.out.println(a1.num1 + " , " + a2.num1);
 		a2.num1 = 20;
 		System.out.println(a1.num1 + " , " + a2.num1);
+		/* 참조 변수를 복사하려면 복사 생성자를 이용하여 객체를 생성한 후
+		 * 맴버 변수들을 복사하려는 객체의 맴버변수로 초기화
+		 */
 		System.out.println("=============================");
 		A a3 = new A(a1);
 		System.out.println(a1.num1 + " , " + a3.num1);
