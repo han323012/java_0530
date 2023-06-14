9cccf4607d7bef6d497f0f5708c4ab4a9b2679ea
han323012 <134986656+han323012@users.noreply.github.com>
2023-06-14 14:54:58 +0900

    11일차 숙제

---
+++ java/src/day11/homework/BaseballGameEx.java
@@ -0,0 +1,13 @@
+package day11.homework;
+
+public class BaseballGameEx {
+
+	public static void main(String[] args) {
+		//숫자 야구 게임을 구현하세요
+		//1~9사이의 중복되지 않은 세 수를 맟추는 게임
+		//b 같은 숫자가 있지만 위치가 다름
+		//s 같은 숫자 같은 위치
+		//o 다른 숫자
+	}
+
+}
