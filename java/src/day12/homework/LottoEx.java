1999c18c2001c17976f5869e210300f35c5b83d4
han323012 <134986656+han323012@users.noreply.github.com>
2023-06-16 12:38:22 +0900

    12일차 숙제

--- java/src/day12/homework/LottoEx.java
+++ java/src/day12/homework/LottoEx.java
@@ -33,6 +33,7 @@ import array.Array;
 public class LottoEx {
 
 	public static void main(String[] args) {
+
 		int arr1[] = new int[7];
 		int arr2[] = new int[6];
 		int count=0;
@@ -54,8 +55,57 @@ public class LottoEx {
 		else if(count ==3) System.out.println("5등");
 		else System.out.println("꽝");
 		
+		/*
+		 * int lotto[] = new int[7];
+		Array.createRandomArray(1, 45, lotto);
 		
-		
-		
+		int bonus =lotto[6];
+		int lotto2 = new int[6];
+		System.arraycopy(lotto, 0, lotto2, 0, 0);
+		*/
+	}
+	/**로또번호와 사용자 번호가 주어지면 몇개가 같은지 알려주는 메서드
+	 * 매새변수 int lotto[] int user[]
+	 * 리턴타입 int
+	 * 메서드명 count
+	 */
+	public static int count(int lotto[],int user[]) {
+		if(lotto == null || user == null) {
+			return 0;
+		}
+		int count = 0;
+		for(int tmp : lotto) {
+			if(Array.contains(user.length, tmp, user)) {
+				count++;
+			}
+		}
+		return count;
+	}
+	/**일치하는 개수와 보너스 번호와 사용자 번호가 일치하는 여부를 알려주면 등수를 출력하는 메소드
+	 * 매개변수 int count boolean checkBonus
+	 * 리턴타입
+	 * 메서드명	
+	 */
+	public static void printResult(int count, boolean checkBonus) {
+		switch(count) {
+		case 6:
+			System.out.println("Winner of the lottery");
+			break;
+		case 5:
+			if(checkBonus) {
+			System.out.println("second place in the lottery");
+			}else {
+			System.out.println("3rd place in the lottery");
+			}
+			break;
+		case 4:
+			System.out.println("4rd place in the lottery");
+			break;
+		case 3:
+			System.out.println("5rd place in the lottery");
+			break;
+		default:
+		System.out.println("losing ticket");
+		}
 	}
 }
