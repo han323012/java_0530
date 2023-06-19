3a5fc64236740f5fff9c3a1dcce0745ac041252b
han323012 <134986656+han323012@users.noreply.github.com>
2023-06-19 11:43:17 +0900

    13일차 숙제

--- java/src/day13/homework/VocabularyNoteEx.java
+++ java/src/day13/homework/VocabularyNoteEx.java
@@ -1,5 +1,7 @@
 package day13.homework;
 
+import day13.homework.vo.Word;
+
 public class VocabularyNoteEx {
 
 	public static void main(String[] args) {
@@ -11,8 +13,10 @@ public class VocabularyNoteEx {
 		단어수정 뜻수정 뜻삭제
 		4단어출력
 		전체출력 검색
-		5프로그램종료
-		*/
+		5프로그램종료*/
+		/*
+		 * 영어 단어장 클래스를 만들고 테스트 하는 코드를 작성하세요.
+		 */
+		
 	}
-
 }
---
+++ java/src/day13/homework/run/Run.java
@@ -0,0 +1,12 @@
+package day13.homework.run;
+
+import day13.homework.controller.VocabularyNoteContoller;
+
+public class Run {
+
+	public static void main(String[] args) {
+		VocabularyNoteContoller controller =new VocabularyNoteContoller();
+		controller.run();
+	}
+
+}
