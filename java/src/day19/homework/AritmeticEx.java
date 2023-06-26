fe007443884244400ad0af69962ac7ab3d7e4134
han323012 <134986656+han323012@users.noreply.github.com>
2023-06-26 14:56:27 +0900

    19일차 숙제

--- java/src/day19/homework/AritmeticEx.java
+++ java/src/day19/homework/AritmeticEx.java
@@ -1,10 +1,8 @@
 package day19.homework;
 
 public class AritmeticEx {
-
+//
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//산술연산하는 메서드를 만들고 테스트하는 코드를 작성하세요 단 예외처리 이용.
 	}
-
 }
